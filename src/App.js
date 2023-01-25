8f484b5300eb4f0fab17eccb24a21718c34d7497
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-01-25 15:41:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -23,3 +23,16 @@ function App() {
 }
 
 export default App;
+
+// {
+//   "clientId": "65611a17-fcdc-4262-81c2-d11823de3838",
+//   "clientSecret": "o688Q~xl9cjHb.ava-.3dajbo6NGl57OKN~U3bp6",
+//   "subscriptionId": "351b6525-81b1-48bf-a6e8-4bd5da72e974",
+//   "tenantId": "39de4c31-5af6-43b4-8418-bb6a45179340",
+//   "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+//   "resourceManagerEndpointUrl": "https://management.azure.com/",
+//   "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+//   "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+//   "galleryEndpointUrl": "https://gallery.azure.com/",
+//   "managementEndpointUrl": "https://management.core.windows.net/"
+// }
